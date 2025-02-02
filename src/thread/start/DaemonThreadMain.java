7d3c20a8e16550fba5554c390909b52570145cee
JHyun0302 <wogustkfkd11@gmail.com>
2025-02-02 22:02:33 +0900

    thread 시작

---
+++ src/thread/start/DaemonThreadMain.java
@@ -0,0 +1,26 @@
+package thread.start;
+
+public class DaemonThreadMain {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+        DaemonThread daemonThread = new DaemonThread();
+        daemonThread.setDaemon(true); // 데몬 스레드 여부 (true: 바로 꺼짐(User Thread가 기본), false: DaemonThread 까지 작업이 끝나야 종료(10초))
+        daemonThread.start();
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+
+    static class DaemonThread extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ": run()");
+            try {
+                Thread.sleep(10000); // 10초간 실행
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + ": run() end");
+        }
+    }
+}
---
+++ src/thread/start/test/StartTest3Main.java
@@ -0,0 +1,27 @@
+package thread.start.test;
+
+import static util.MyLogger.log;
+
+import thread.start.test.StartTest2Main.CounterRunnable;
+
+public class StartTest3Main {
+
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    log("value: " + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
+        Thread thread = new Thread(runnable, "counter");
+        thread.start();
+    }
+}
